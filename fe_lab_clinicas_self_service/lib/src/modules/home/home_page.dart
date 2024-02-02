dfeb08b311ba9007337644100bc76ec54cb00a7f
Eduardo Pereira <eduardo.souzapereira@gmail.com>
2024-02-02 14:43:47 -0300

    create auto atendimento - home page

--- fe_lab_clinicas_self_service/lib/src/modules/home/home_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,11 +6,59 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      appBar: LabClinicasAppBar(
+        actions: [
+          PopupMenuButton<int>(
+              child: const IconPopupMenuWidget(),
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem<int>(
+                    value: 1,
+                    child: Text('Iniciar Terminal'),
+                  ),
+                  const PopupMenuItem<int>(
+                    value: 2,
+                    child: Text('Finalizar Terminal'),
+                  ),
+                ];
+              })
+        ],
       ),
-      body: Container(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          margin: const EdgeInsets.only(top: 112),
+          padding: const EdgeInsets.all(40),
+          width: sizeOf.width * .8,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(color: LabClinicasTheme.orangeColor),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Bem Vindo!',
+                style: LabClinicasTheme.titleStyle,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: sizeOf.width * .8,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('INICIAR TERMINAL'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
