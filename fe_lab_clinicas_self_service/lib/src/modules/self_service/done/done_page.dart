7f58a53662a0699c6bfe82dfefbb4f39e7b3fac7
Eduardo Pereira <eduardo.souzapereira@gmail.com>
2024-02-05 19:01:00 -0300

    auto atendimento - create done page

--- fe_lab_clinicas_self_service/lib/src/modules/self_service/done/done_page.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/done/done_page.dart
@@ -1,8 +1,13 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 
 class DonePage extends StatelessWidget {
-  const DonePage({super.key});
+  final SelfServiceController selfServiceController =
+      Injector.get<SelfServiceController>();
+
+  DonePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +39,89 @@ class DonePage extends StatelessWidget {
                   height: 24,
                 ),
                 Container(
-                  width: 218,
-                  height: 48,
+                  constraints: const BoxConstraints(
+                    minWidth: 218,
+                    minHeight: 48,
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
                   decoration: BoxDecoration(
                     color: LabClinicasTheme.orangeColor,
                     borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Text(
+                    selfServiceController.password,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text.rich(
+                  TextSpan(
+                    style: TextStyle(
+                      color: LabClinicasTheme.blueColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: [
+                      TextSpan(text: 'AGUARDE!\n'),
+                      TextSpan(text: 'Sua senha será chamada no painel'),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('IMPRIMIR SENHA')),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 48,
+                        child: OutlinedButton(
+                            onPressed: () {},
+                            child: const Text('ENVIAR SENHA VIA SMS')),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 48,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: LabClinicasTheme.orangeColor,
+                    ),
+                    onPressed: () {
+                      selfServiceController.restartProcess();
+                    },
+                    child: const Text(
+                      'FINALIZAR',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ),
               ],
--- fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_controller.dart
+++ fe_lab_clinicas_self_service/lib/src/modules/self_service/self_service_controller.dart
@@ -1,7 +1,10 @@
+import 'package:asyncstate/asyncstate.dart';
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:signals_flutter/signals_flutter.dart';
+
 import 'package:fe_lab_clinicas_self_service/src/model/patient_model.dart';
 import 'package:fe_lab_clinicas_self_service/src/model/self_service_model.dart';
-import 'package:signals_flutter/signals_flutter.dart';
+import 'package:fe_lab_clinicas_self_service/src/repositories/information_form/information_form_repository.dart';
 
 enum FormSteps {
   none,
@@ -14,10 +17,17 @@ enum FormSteps {
 }
 
 class SelfServiceController with MessageStateMixin {
+  SelfServiceController({
+    required this.informationFormRepository,
+  });
+
+  final InformationFormRepository informationFormRepository;
+
   final _step = ValueSignal(FormSteps.none);
   var _model = const SelfServiceModel();
-  SelfServiceModel get model => _model;
+  var password = '';
 
+  SelfServiceModel get model => _model;
   FormSteps get step => _step();
 
   void startProcess() {
@@ -62,5 +72,17 @@ class SelfServiceController with MessageStateMixin {
 
   void clearDocuments() {
     _model = _model.copyWith(documents: () => {});
+  }
+
+  Future<void> finalize() async {
+    final result =
+        await informationFormRepository.register(model).asyncLoader();
+    switch (result) {
+      case Left():
+        showError('Erro ao registrar atendimento');
+      case Right():
+        password = '${_model.name} ${_model.lastName}';
+        _step.forceUpdate(FormSteps.done);
+    }
   }
 }
