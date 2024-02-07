00888cf2803e64419ef377e2aeb64883e052bdc6
Eduardo Pereira <eduardo.souzapereira@gmail.com>
2024-02-06 21:52:52 -0300

    adm - finish home page

--- fe_lab_clinicas_adm/lib/src/pages/home/home_router.dart
+++ fe_lab_clinicas_adm/lib/src/pages/home/home_router.dart
@@ -1,6 +1,4 @@
 import 'package:fe_lab_clinicas_adm/src/pages/home/home_controller.dart';
-import 'package:fe_lab_clinicas_adm/src/repositories/attendant_desk_assignment/attendant_desk_assignment_repository.dart';
-import 'package:fe_lab_clinicas_adm/src/repositories/attendant_desk_assignment/attendant_desk_assignment_repository_impl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'home_page.dart';
@@ -10,10 +8,9 @@ class HomeRouter extends FlutterGetItPageRouter {
 
   @override
   List<Bind<Object>> get bindings => [
-        Bind.lazySingleton<AttendantDeskAssignmentRepository>(
-            (i) => AttendantDeskAssignmentRepositoryImpl(restClient: i())),
-        Bind.lazySingleton(
-            (i) => HomeController(attendantDeskAssignmentRepository: i())),
+        Bind.lazySingleton((i) => HomeController(
+            attendantDeskAssignmentRepository: i(),
+            callNextPatientService: i())),
       ];
 
   @override
